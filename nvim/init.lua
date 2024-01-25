99caed16a1a6fe4f3e65bbbe15bcc2bae89be1b4
musa gillani <musagillaniwork@gmail.com>
2024-01-26 02:29:47 +0500

    nvim config: lazy.nvim setup catppuccin colorscheme setup

---
+++ nvim/init.lua
@@ -0,0 +1,25 @@
+-- setup lazy nvim as package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- defining catpuccin plugin to be installed 
+
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }		
+}
+local opts = {}
+require("lazy").setup(plugins, opts)
+
+-- setup catppuccin 
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
