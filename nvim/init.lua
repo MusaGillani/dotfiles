f87e408812b12c8c754f3a4d64588a9464cba6e8
musa gillani <musagillaniwork@gmail.com>
2024-01-26 03:25:00 +0500

    setup telescope for fuzzy finding in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+-- setup leader key 
+vim.g.mapleader = " "
 -- setup lazy nvim as package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -12,10 +14,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- defining catpuccin plugin to be installed 
-
+-- plugins to install 
 local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }		
+	-- defining catpuccin plugin to be installed 
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	-- telescope for fuzzy finding and live grep
+	{
+    		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      		dependencies = { 'nvim-lua/plenary.nvim' }
+    	}
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
@@ -23,3 +30,12 @@ require("lazy").setup(plugins, opts)
 -- setup catppuccin 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+-- setup telescope
+local builtin = require("telescope.builtin")
+-- use Ctrl-p to find files
+vim.keymap.set("n","<C-p>", builtin.find_files,{})
+-- run :checkhealth telescope, might error for missing dep ripgrep
+-- install that using brew: brew install ripgrep and run checkhealth again
+-- ripgrep is required for live_grep to work
+vim.keymap.set("n","<leader>fg", builtin.live_grep,{})
